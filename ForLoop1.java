0bdc7a88d2a71d2834665bf2da1ef46e93b7574a
pallavi Mane <109073274+Palla686@users.noreply.github.com>
2022-07-19 05:36:55 -0700

    Add files via upload

---
+++ ForLoop1.java
@@ -0,0 +1,17 @@
+class ForLoop1
+{
+	
+public static void main(String[] args)
+	
+{
+System.out.println("Printing the value from 10 to 6");		
+for(int i=10; i>6; i--)	
+		
+{
+			
+System.out.println(" The value of i:"+ i);
+			
+}
+	
+}
+}	
