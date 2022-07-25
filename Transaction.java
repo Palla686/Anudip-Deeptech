73d4c43c881ace1ba65327ca9ea0790078b9def3
pallavi Mane <109073274+Palla686@users.noreply.github.com>
2022-07-25 05:35:16 -0700

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,92 @@
+class Payment
+
+{
+	
+ boolean payment;
+	
+ public void status()
+{
+		
+ payment=false;
+		
+ System.out.println("PAYMENT STATUS:"+payment);	
+
+	
+}
+
+}
+
+ class NetBanking extends Payment
+
+{
+	
+ public void netBank()
+	
+{
+		
+  System.out.println("PAYMENT Type: NetBanking");
+		
+	
+}
+
+
+
+}
+
+ class DebitCard extends Payment
+
+{
+	
+  public void debitCard()
+
+{ 		
+ System.out.println("PAYMENT Type:DebitCard");
+		
+	
+}
+
+
+
+}
+
+ class UPI extends Payment
+
+{
+	
+  public void UPI()
+	
+{
+		
+  System.out.println("PAYMENT Type:UPI");
+		
+	
+}
+
+
+
+}
+
+ class Transaction
+
+{
+	
+ public static void main(String[] ar)
+	
+ {
+
+		
+  DebitCard p2=new DebitCard();
+		
+  p2.debitCard();
+		
+  UPI p1=new UPI();
+		
+  p1.UPI();
+		
+   NetBanking p=new NetBanking();
+		
+  p.status();
+	
+ 
+
+}}
